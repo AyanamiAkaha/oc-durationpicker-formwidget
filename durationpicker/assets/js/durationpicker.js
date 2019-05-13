400cbb748e62149f89fffce98d0ebbd417bd2b9c
Ayanami Akaha <ayanami.eve00@gmail.com>
2019-05-13 18:30:07 +0000

    Fix updating inputs
    Input updates was not handled properly, if for example minutes input
    added over a day, and day input was not touched. Changed the logic to
    always update all inputs. Additionally moved to using ES6 class for
    handling each widget, as browser compatibility is good enough for my use
    cases.

--- durationpicker/assets/js/durationpicker.js
+++ durationpicker/assets/js/durationpicker.js
@@ -3,74 +3,79 @@
     if(n >= 10) return n.toString();
     return `0${n}`;
   }
-  function normalizeDuration($input, momentDuration) {
-    if(!momentDuration) {
-      momentDuration = moment.duration($input.duration);
-      if(!momentDuration.isValid()) {
-        momentDuration = moment.duration($input.value);
+  // keep reference to prevent GC collecting instances
+  const durationPickers = [];
+
+  class DurationPicker {
+    constructor($input, $days, $hours, $minutes, $seconds) {
+      this.$input = $input;
+      this.$d = $days || null;
+      this.$h = $hours || null;
+      this.$m = $minutes || null;
+      this.$s = $seconds || null;
+
+      this.momentDuration = moment.duration($input.value);
+
+      this.populateInputs();
+      if(this.$d) this.$d.addEventListener('change', this.inputChange.bind(this));
+      if(this.$h) this.$h.addEventListener('change', this.inputChange.bind(this));
+      if(this.$m) this.$m.addEventListener('change', this.inputChange.bind(this));
+      if(this.$s) this.$s.addEventListener('change', this.inputChange.bind(this));
+    }
+
+    inputChange() {
+      this.momentDuration = moment.duration({
+        days: this.$d ? Number(this.$d.value) : 0,
+        hours: this.$h ? Number(this.$h.value) : 0,
+        minutes: this.$m ? Number(this.$m.value) : 0,
+        seconds: this.$s ? Number(this.$s.value) : 0,
+      });
+      this.populateInputs();
+    }
+    populateInputs() {
+      if(this.momentDuration.isValid()) {
+        const d = this.momentDuration.days();
+        let h = this.momentDuration.hours();
+        let m = this.momentDuration.minutes();
+        let s = this.momentDuration.seconds();
+        // missing larger inputs will be handled properly,
+        // but missing smaller ones get ignored. Visually it's just
+        // truncating fractional value in the smallest input
+        if(this.$d) {
+          this.$d.value = d;
+        } else {
+          h += d*24;
+        }
+        if(this.$h) {
+          this.$h.value = h;
+        } else {
+          m += h*60;
+        }
+        if(this.$m) {
+          this.$m.value = m;
+        } else {
+          s += m*60;
+        }
+        if(this.$s) this.$s.value = s;
+        // re-calculate, since d/h/m/s might have got modified above
+        const hstr = zeroFill(this.momentDuration.days()*24 + this.momentDuration.hours());
+        const mstr = zeroFill(this.momentDuration.minutes());
+        const sstr = zeroFill(this.momentDuration.seconds());
+        this.$input.value = `${hstr}:${mstr}:${sstr}`;
       }
-      if(!momentDuration.isValid()) return; // don't parse invalid durations
     }
-    $input.duration = {
-      days: momentDuration.days(),
-      hours: momentDuration.hours(),
-      minutes: momentDuration.minutes(),
-      seconds: momentDuration.seconds(),
-    };
   }
-  function inputChange($input, $el, interval) {
-    const n = Number($el.value);
-    if(!Number.isNaN(n)) {
-      $input.duration[interval] = n;
-    }
-    normalizeDuration($input);
-    const h = zeroFill($input.duration.days*24+$input.duration.hours);
-    const m = zeroFill($input.duration.minutes);
-    const s = zeroFill($input.duration.seconds);
-    $input.value = `${h}:${m}:${s}`;
-  }
+
   window.addEventListener('load', function() {
     document
       .querySelectorAll('.field-durationpicker')
       .forEach(($el) => {
-        console.log($el);
         const $days = $el.querySelector('[data-duration-days]');
         const $hours = $el.querySelector('[data-duration-hours]');
         const $minutes = $el.querySelector('[data-duration-minutes]');
         const $seconds = $el.querySelector('[data-duration-second]');
         const $input = $el.querySelector('input[type=hidden]');
-        const momentDuration = moment.duration($input.value);
-        if(momentDuration.isValid) {
-          $input.duration = {
-            days: momentDuration.days(),
-            hours: momentDuration.hours(),
-            minutes: momentDuration.minutes(),
-            seconds: momentDuration.seconds(),
-          };
-        } else {
-          $input.duration = {
-            days: 0,
-            hours: 0,
-            minutes: 0,
-            seconds: 0,
-          };
-        }
-        if($days) {
-          $days.value = $input.duration.days;
-          $days.addEventListener('change', () => inputChange($input, $days, 'days'));
-        }
-        if($hours) {
-          $hours.value = $input.duration.hours;
-          $hours.addEventListener('change', () => inputChange($input, $hours, 'hours'));
-        }
-        if($minutes) {
-          $minutes.value = $input.duration.minutes;
-          $minutes.addEventListener('change', () => inputChange($input, $minutes, 'minutes'));
-        }
-        if($seconds) {
-          $seconds.value = $input.duration.seconds;
-          $seconds.addEventListener('change', () => inputChange($input, $seconds, 'seconds'));
-        }
+        durationPickers.push(new DurationPicker($input, $days, $hours, $minutes, $seconds));
       });
   });
 }())
